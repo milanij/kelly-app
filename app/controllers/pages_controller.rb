25362ed53cc397024955e6c9e5b871f27dc61c58
James Milani <james.milani@gmail.com>
2016-03-08 17:06:45 -0800

    Add some images and get the styling of the about page going.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,27 @@
 class PagesController < ApplicationController
 
-  def homepage
-    render "homepage", layout: "layouts/application"
+  def about
+    render "about", layout: "layouts/application"
+  end
+
+  def news
+    render "news", layout: "layouts/application"
+  end
+
+  def calendar
+    render "calendar", layout: "layouts/application"
+  end
+
+  def photos
+    render "photos", layout: "layouts/application"
+  end
+
+  def videos
+    render "videos", layout: "layouts/application"
+  end
+
+  def contact
+    render "contact", layout: "layouts/application"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,26 @@ Rails.application.routes.draw do
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
 
-  root to: "pages#homepage"
+  root to: "pages#about"
+
+  get "/news",
+    to: "pages#news",
+    as: :news_path
+
+  get "/calendar",
+    to: "pages#calendar",
+    as: :calendar_path
+
+  get "/photos",
+    to: "pages#photos",
+    as: :photos_path
+
+  get "/videos",
+    to: "pages#videos",
+    as: :videos_path
+
+  get "/contact",
+    to: "pages#contact",
+    as: :contact_path
+
 end
